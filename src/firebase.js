8f54c155ab05e90d411faaf179e5b8616670567c
aniketL7 <64003302+aniketL7@users.noreply.github.com>
2021-05-28 08:50:58 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,16 +3,6 @@ import "firebase/auth";
 import "firebase/firestore";
 
 
-firebase.initializeApp({
-    apiKey: "AIzaSyAGB4E3JHe_4cK7dZSxeF0CO5hunTdAInc",
-    authDomain: "test-project-897ae.firebaseapp.com",
-    databaseURL: "https://test-project-897ae-default-rtdb.firebaseio.com",
-    projectId: "test-project-897ae",
-    storageBucket: "test-project-897ae.appspot.com",
-    messagingSenderId: "12292061419",
-    appId: "1:12292061419:web:3f6ddafb4c89fa9ae5e6ae",
-    measurementId: "G-STWXB7DG7T"
-});
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
